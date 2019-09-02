15a2c6592abee00349ba7bf7a607a8a27a6bdd9a
Dinidh O'Brien <info@gradivis.com>
2019-09-02 14:41:41 +0200

    Script update

--- Scripts/brix-baume-conversion.py
+++ Scripts/brix-baume-conversion.py
@@ -1,10 +1,16 @@
 #Mum's Brix to Baume Converter with input
 print("Welcome to the Brix to Baumé converter.")
-brix = int(input("Please enter the refractometer reading in brix: "))
+while True:      
+    try:
+        brix = float(input("Please enter the refractometer reading in brix: "))
+        break    
+    except ValueError:
+        print("Please enter a number value!")
+
 baume = (brix + 1.6)/1.905
 print(f"{brix}\u00b0 brix is {baume:.4}\u00b0 in baumé.")
 
 if baume >= 10:
-    print("These grapes are ready for harvest!")
+    print("These grapes are ready for harvest!")    
 else: 
-    print("The baumé level is still to low for harvest.")
+    print("The baumé level is still too low for harvest.")
