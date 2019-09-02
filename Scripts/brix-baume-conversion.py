b487cb1f29df57c11e822678be3d54a093a100a7
Dinidh O'Brien <info@gradivis.com>
2019-09-02 10:32:17 +0200

    Brix to Baume Initial Script

---
+++ Scripts/brix-baume-conversion.py
@@ -0,0 +1,9 @@
+#Mum's Brix to Baume Converter
+brix = 40
+baume = (brix + 1.6)/1.905
+print(f"{brix}\u00b0 brix is {baume:.4}\u00b0 in baumé.")
+
+if baume >= 10:
+    print("These grapes are ready for harvest!")
+else: 
+    print("The baumé level is still to low for harvest.")
