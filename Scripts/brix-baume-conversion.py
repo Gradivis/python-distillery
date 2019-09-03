821bca987888ef9614f5b5c67a4e9b55bbfe63f9
Dinidh O'Brien <info@gradivis.com>
2019-09-03 20:38:48 +0200

    Learnt about functions and made one docstrings inc.

--- Scripts/brix-baume-conversion.py
+++ Scripts/brix-baume-conversion.py
@@ -1,4 +1,15 @@
 #Mum's Brix to Baume Converter with input
+def is_ripe(baume):
+	'''
+    This fucntion will parse the baume conversion 
+    and determine whether grapes are right or not.
+    INPUT: Baume Calculation
+    '''
+    if baume >= 10:
+        print('Grapes are ready to pick!')
+    elif baume < 10:
+        print('Whoops, not ready to pick yet!')
+
 print("Welcome to the Brix to Baumé converter.")
 while True:      
     try:
@@ -9,8 +20,7 @@ while True:
 
 baume = (brix + 1.6)/1.905
 print(f"{brix}\u00b0 brix is {baume:.4}\u00b0 in baumé.")
+is_ripe(baume)
 
-if baume >= 10:
-    print("These grapes are ready for harvest!")    
-else: 
-    print("The baumé level is still too low for harvest.")
+
+    
